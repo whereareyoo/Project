8f021b79b02b763224edef7e8bd0df8709f5d5f8
whereareyoo <ramin-gashimov@mail.ru>
2023-02-02 17:28:45 +0200

    Changes 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import Catalog from "../../features/catalog/catalog";
+import { Product } from "../models/product";
+
+
+function App() {
+  const[products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  },[])
+
+  function addProduct(){
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 101,
+      name: 'product' + (prevState.length + 1),
+      price: (prevState.length * 100) + 100,
+      brand: 'some brand',
+      description: 'something',
+      picUrl: 'http://picsum.photos/200',
+
+    }])
+  }
+  return (
+    <>
+      <Typography variant = 'h1'>Re-Store</Typography>
+      <Catalog products={products} addProduct={addProduct} />
+      
+    </>
+  );
+}
+
+
+export default App;
---
+++ client/src/features/catalog/catalog.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props{
+    products: Product[];
+    addProduct: () => void;
+}
+
+
+
+export default function Catalog({products, addProduct}: Props)
+{
+    return(
+        <>
+            <List>
+                {products.map(product => (
+                <ListItem key = {product.id}>
+                    <ListItemAvatar>
+                        <Avatar src = {product.picUrl}/>
+                    </ListItemAvatar>
+                    <ListItemText>
+                        {product.name} - {product.price}
+                    </ListItemText>
+                </ListItem>
+                ))}
+            </List>
+            <Button variant='contained' onClick = {addProduct}>Add product</Button>
+        </>
+        
+    )
+}
