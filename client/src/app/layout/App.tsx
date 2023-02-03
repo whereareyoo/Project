e3e8f2b293f3d35982b4d2bb7f7a7e8dc6aa37c5
whereareyoo <ramin-gashimov@mail.ru>
2023-02-03 23:20:31 +0200

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,6 +4,12 @@ import Header from "./Header";
 import { Container, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -30,7 +36,11 @@ function handleThemeChange()
         <CssBaseline />
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Container>
-        <Catalog />
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route exact path='/catalog/:id' component={ProductDetails} />
+          <Route exact path='/about' component={AboutPage} />
+          <Route exact path='/contact' component={ContactPage} />
         </Container>
       </ThemeProvider>
   );
